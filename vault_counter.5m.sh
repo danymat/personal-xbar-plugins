c9b02d13e755cbc3db5691485fa4ed301997eaac
Daniel Mathiot <d.danymat@gmail.com>
2021-04-02 20:58:58 +0200

    feat: Added todos for vault

--- vault_counter.5m.sh
+++ vault_counter.5m.sh
@@ -8,14 +8,32 @@
 #  <xbar.author.github>danymat</xbar.author.github>
 #  <xbar.desc>Display the number of notes in the specified location</xbar.desc>
 #  <xbar.image>https://raw.githubusercontent.com/danymat/personal-xbar-plugins/main/images/obsidian_2021_03_30.png</xbar.image>
-#  <xbar.var>string(vault="/Users/danielmathiot/.config/skhd/"): The location of your current vault.</xbar.var>
+#  <xbar.var>string(vault="/Users/danielmathiot/vault"): The location of your current vault.</xbar.var>
 #  <xbar.dependencies></xbar.dependencies>
 #  <xbar.abouturl></xbar.abouturl>
 
 number_notes=$(ls -p "$vault" | grep -v / | wc -l)
 vault_name_uri=$(echo $vault |  sed -e 's:.*/::' -e 's/ /%20/g')
+files_todos=$(grep -R --exclude-dir=.obsidian TODO "$vault" | cut -d ':' -f 1 | uniq | sed -e 's/.*\///g' -e "s|^|$vault/|g")
 
 echo $number_notes Notes
 echo "---"
 echo "Vault folder: $vault"
+echo "---"
+echo "TODOS"
+while IFS= read -r file
+do
+    file_name=$(echo "$file" | sed 's|.*/||')
+    #file_name_uri=$(echo "$file" | sed -e 's:.*/::' -e 's/ /%20/g' -e "s/'/%39/g")
+    echo "$file_name" # | href=obsidian://open?vault=$vault_name_uri&file=$file_name_uri"
+    todos=$(grep 'TODO' "$file" | sed -e 's/^[^%]*%*TODO: /--/g' -e 's/%%//g') # keep only what inside %%TODO: %%)
+    echo "$todos"
+done <<< "$files_todos"
+echo "---"
+# while read -r file ; do
+#     echo "salut$file"
+#     # todos=$(grep 'TODO' "$file" | sed -e 's/^[^%]*%*TODO: /--/g' -e 's/%%//g') # keep only what inside %%TODO: %%)
+#     # echo "$todos"
+# done < "$files_todos"
+
 echo "Open Obsidian vault | href=obsidian://open?vault=$vault_name_uri"
